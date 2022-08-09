e86271a4a9ac31e34b955581617d35c4e1f6fc7c
Marcuso93 <103344643+Marcuso93@users.noreply.github.com>
2022-08-08 22:51:03 -0500

    Updated code

--- Tic-Tac-Toe/index.js
+++ Tic-Tac-Toe/index.js
@@ -1,15 +1,14 @@
-///----VARIABLES----
-let gameBoard= document.querySelector("#game-board")
+let gameBoard = document.querySelector("#game-board")
 let winner = document.querySelector("#game-winner");
 let playerElem = document.querySelector(".player-name");
 
-///----GAME STATE-----
-
 let game = {
+    board: [],
     players: ['', ''],
+    currentLetter: 'X',
     gameOver: false,
     gameStarted:false,
-    currentLetter: 'X',
+    turns: 0,
     winningLine: [
         [0, 1, 2],
         [3, 4, 5],
@@ -21,136 +20,141 @@ let game = {
         [2, 4, 6],
     ]
 }
-function newMark () {
-    if (game.currentLetter ==="X") {
-        game.currentLetter = "0";
+
+function setGame() {
+    game.board = [
+        { value: '', boxClicked: false },
+        { value: '', boxClicked: false },
+        { value: '', boxClicked: false },
+        { value: '', boxClicked: false },
+        { value: '', boxClicked: false },
+        { value: '', boxClicked: false },
+        { value: '', boxClicked: false },
+        { value: '', boxClicked: false },
+        { value: '', boxClicked: false },];
+    game.players = ['', ''];
+    game.turns = 0;
+    game.currentPlayerIdx = 0;
+    game.getCurrentPlayer = () => game.players[game.currentPlayerIdx];
+    winner.innerHTML = " "
+}
+
+function newMark() {
+    if (game.currentLetter === "X") {
+        game.currentLetter = "O";
     } else {
-        game.currentLetter ="X";
+        game.currentLetter = "X";
     }
 }
 
-//----------------
-function setGame () {
-    game.board = [
-        {value: '', boxClicked: false},
-        {value: '', boxClicked: false},
-        {value: '', boxClicked: false},
-        {value: '', boxClicked: false},
-        {value: '', boxClicked: false},
-        {value: '', boxClicked: false},
-        {value: '', boxClicked: false},
-        {value: '', boxClicked: false},
-        {value: '', boxClicked: false},];
-        game.players = ['', ''];
-        game.currentPlayerIdx = 0;
-        game.getCurrentPlayer  = () => game.players[game.currentPlayerIdx];
-        
-}
-
 const changeTurn = () => {
-     game.currentPlayerIdx = Math.abs(game.currentPlayerIdx - 1);
-    
-  };
+    game.currentPlayerIdx = Math.abs(game.currentPlayerIdx - 1);
+};
 
-//-------------RENDER------------------------
+function render() {
+    gameBoard.innerHTML = '';
 
- function render() {
-     gameBoard.innerHTML = '';
-
-     for (let i = 0; i < game.board.length; i++) {
-
-       let box = document.createElement('div');
-        box.innerHTML= game.board[i].value;
+    for (let i = 0; i < game.board.length; i++) {
+        let box = document.createElement('div');
+        box.innerHTML = game.board[i].value;
         box.className = 'box';
         box.dataset.index = i;
-
         gameBoard.appendChild(box);
+        renderPlayer()
+    }
+}
 
-         }
-     }
-
-function renderPlayer () {
-        let text;
-        if (!game.players[0] && !game.players[1]) {
-          text = `
+function renderPlayer() {
+    let text;
+    if (!game.players[0] && !game.players[1]) {
+        text = `
           <input id='player1' placeholder="Player One's Name">
           <input id='player2' placeholder="Player Two's Name">
           <button class='start' onclick="startBtn()">Start Game</button>
+          <h6> *Enter only in Player One's Name if playing single player*</h6>
           `;
-        } else {
-          text = `It's now ${game.getCurrentPlayer()}'s turn`;
+    } else {
+        text = `It's now ${game.getCurrentPlayer()}'s turn`;
+    }
+    playerElem.innerHTML = text;
+};
+
+function computerPlayer() {
+    if (game.players[1] = 'Computer') {
+        changeTurn(newMark);
+        game.turns++;
+    }
+}
+
+function playersTurn(boxIdx) {
+    if (game.currentPlayerIdx === 0) {
+        mark = ' X';
+    }
+    else {
+        mark = 'O';
+    }
+
+    currentItem = game.board[boxIdx];
+    if (currentItem.boxClicked === false) {
+        game.board[boxIdx].value = mark;
+        currentItem.boxClicked = true;
+        changeTurn();
+        if (wonGame(mark)) {
+            winner.innerHTML = "Game Over! Got a Winner!";
         }
-        playerElem.innerHTML = text;
-        
-      };
+    }
+}
 
-//-------------------------------------
-
- function playersTurn(boxIdx) {
-     if (game.currentPlayerIdx === 0) {
-         mark = ' X';
-     }
-        else {
-            mark = 'O';
-        }
-
-     currentItem = game.board[boxIdx];
-        if (currentItem.boxClicked === false) {
-                 game.board[boxIdx].value = mark;
-                 currentItem.boxClicked = true;
-                 if (wonGame(mark))  {
-                    winner.innerHTML = "Game Over! Got a Winner!";
-                }
-                 changeTurn();
-             }
-          }
-
-function wonGame (mark) {
-    const marks = mark.repeat(3);
+function wonGame(mark) {
+    let marks = mark.repeat(3);
     for (let row of game.winningLine) {
         let rowMarks = "";
         for (let index of row) {
             rowMarks += game.board[index].value;
         }
-        if (rowMarks ===marks) return row;
+        if (rowMarks === marks)
+            return row;
     }
-    return false;
 }
 
-//------
+gameBoard.addEventListener('click', function ({ target }) {
+    let boxIdx = target.dataset.index;
 
-gameBoard.addEventListener('click', function({target}) {
     if (!game.started && !game.gameOver) {
-        if (target.className ==='box') {
-            let boxIdx = target.dataset.index;
+        if (target.className === 'box') {
             playersTurn(boxIdx)
             render();
             renderPlayer();
         }
+        return
+    }
+
+    if ((game.players[1] === 'Computer')) {
+        computerPlay();
     }
 });
 
-  function startBtn() {
-     let name1 = document.querySelector("#player1").value;
-     let name2 = document.querySelector("#player2").value;
+function startBtn() {
+    let name1 = document.querySelector("#player1").value;
+    let name2 = document.querySelector("#player2").value;
     game.players = [name1, name2];
+    if (!name2) {
+        game.players[0] = name1;
+        game.players[1] = 'Computer';
+    }
     renderPlayer();
-  }
+}
 
-  function resetBtn() {
-      game.board = [];
-        game.players = ['', ''];
-        game.currentPlayerIdx = 0;
-        game.getCurrentPlayer  = () => game.players[game.currentPlayerIdx];
-        winner.innerHTML = " "
-        setGame ();
-        render();
-        renderPlayer();
-  }
+function resetBtn() {
+    winner.innerHTML = ""
+    setGame();
+    render();
+    renderPlayer();
+}
 
-setGame ()
- render()
- renderPlayer()
+setGame()
+render()
+
 
 
 
